33b6e51a6f19bbf64b1966335d188e02b6e89033
Bach Nguyen <26138322+bachnguyenTE@users.noreply.github.com>
2022-12-22 15:15:09 -0600

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -175,6 +175,45 @@ def read_meta_datasets(window):
 
     meta_y.append(y)
     
+    
+    #---------------- New Zealand
+    os.chdir("../NewZealand")
+    labels = pd.read_csv("newzealand_labels.csv")
+    #del labels["id"]
+    labels = labels.set_index("name")
+
+    sdate = date(2022, 1, 1)
+    edate = date(2022, 11, 4)
+    
+    #--- series of graphs and their respective dates
+    delta = edate - sdate
+    dates = [sdate + timedelta(days=i) for i in range(delta.days+1)]
+    dates = [str(date) for date in dates]
+    labels = labels.loc[:,dates]    #labels.sum(1).values>10
+
+    
+    
+    Gs = generate_graphs_tmp(dates,"NZ")
+    gs_adj = [nx.adjacency_matrix(kgs).toarray().T for kgs in Gs]
+
+    labels = labels.loc[list(Gs[0].nodes()),:]
+    
+    meta_labs.append(labels)
+
+    meta_graphs.append(gs_adj)
+
+    features = generate_new_features(Gs ,labels ,dates ,window)
+
+    meta_features.append(features)
+
+    y = list()
+    for i,G in enumerate(Gs):
+        y.append(list())
+        for node in G.nodes():
+            y[i].append(labels.loc[node,dates[i]])
+
+    meta_y.append(y)
+    
     os.chdir("../../code")
 
     return meta_labs, meta_graphs, meta_features, meta_y
