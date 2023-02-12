de6fb25c02af6621f82088a5235e1c7d4939dd73
bachnguyenTE <bach.nguyen.te@gmail.com>
2023-02-12 16:19:02 +0700

    adding run data from ATMGNN

--- code/experiments_multiresolution.py
+++ code/experiments_multiresolution.py
@@ -219,7 +219,7 @@ if __name__ == '__main__':
                                 torch.save({
                                     'state_dict': model.state_dict(),
                                     'optimizer' : optimizer.state_dict(),
-                                }, 'model_best_{}_shift{}.pth.tar'.format(args.model, shift))
+                                }, 'model_best_{}_shift{}_{}.pth.tar'.format(args.model, shift, country))
 
                             scheduler.step(val_loss)
 
@@ -230,7 +230,7 @@ if __name__ == '__main__':
                     test_loss = AverageMeter()
 
                     #print("Loading checkpoint!")
-                    checkpoint = torch.load('model_best_{}_shift{}.pth.tar'.format(args.model, shift))
+                    checkpoint = torch.load('model_best_{}_shift{}_{}.pth.tar'.format(args.model, shift, country))
                     model.load_state_dict(checkpoint['state_dict'])
                     optimizer.load_state_dict(checkpoint['optimizer'])
                     model.eval()
@@ -258,4 +258,6 @@ if __name__ == '__main__':
                 fw.write(str(args.model)+","+str(shift)+",{:.5f}".format(np.mean(result))+",{:.5f}".format(np.std(result))+",{:.5f}".format(mean_absolute_error(y_true, y_pred))+",{:.5f}".format(mean_squared_error(y_true, y_pred))+",{:.5f}".format(mean_squared_error(y_true, y_pred, squared=False))+",{:.5f}".format(r2_score(y_true, y_pred))+"\n")
                 #fw.write(hypers+",{:.5f}".format(np.mean(result))+",{:.5f}".format(np.std(result))+"\n")
                 fw.close()
+                np.savetxt("predict_{}_shift{}_{}.csv".format(args.model, shift, country), y_pred, fmt="%.5f", delimiter=',')
+                np.savetxt("truth_{}_shift{}_{}.csv".format(args.model, shift, country), y_true, fmt="%.5f", delimiter=',')
 
